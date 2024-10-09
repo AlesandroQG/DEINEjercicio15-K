827b38e8ba8fa6fc5f04cbe37a67c8e7ba643cf2
Alesandro <116791603+AlesandroQuiros@users.noreply.github.com>
2024-10-09 12:05:34 +0200

    fin

--- src/main/java/com/alesandro/ejercicio15k/RelojApplication.java
+++ src/main/java/com/alesandro/ejercicio15k/RelojApplication.java
@@ -3,20 +3,40 @@ package com.alesandro.ejercicio15k;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * Clase donde se ejecuta la aplicación principal
+ *
+ * @author alesandroquirosgobbato
+ */
 public class RelojApplication extends Application {
+    /**
+     * {@inheritDoc}
+     *
+     * Función donde se carga y se muestra la ventana de la aplicación
+     *
+     * @param stage
+     */
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(RelojApplication.class.getResource("/fxml/Reloj.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("RELOJ");
+        Scene scene = new Scene(fxmlLoader.load(), 480, 412);
+        stage.setTitle("Reloj");
+        stage.getIcons().add(new Image(RelojApplication.class.getResourceAsStream("/imagenes/reloj.png")));
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
 
+    /**
+     * Función main donde se lanza la aplicación
+     *
+     * @param args parámetros por consola
+     */
     public static void main(String[] args) {
         Application.launch();
     }
--- src/main/java/com/alesandro/ejercicio15k/RelojController.java
+++ src/main/java/com/alesandro/ejercicio15k/RelojController.java
@@ -1,14 +1,104 @@
 package com.alesandro.ejercicio15k;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
 
-public class RelojController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.ResourceBundle;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+import java.time.LocalTime;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Clase que controla los eventos de la ventana
+ */
+public class RelojController implements Initializable {
+    @FXML // fx:id="hora1"
+    private ImageView hora1; // Value injected by FXMLLoader
+
+    @FXML // fx:id="hora2"
+    private ImageView hora2; // Value injected by FXMLLoader
+
+    @FXML // fx:id="min1"
+    private ImageView min1; // Value injected by FXMLLoader
+
+    @FXML // fx:id="min2"
+    private ImageView min2; // Value injected by FXMLLoader
+
+    @FXML // fx:id="seg1"
+    private ImageView seg1; // Value injected by FXMLLoader
+
+    @FXML // fx:id="seg2"
+    private ImageView seg2; // Value injected by FXMLLoader
+
+    /**
+     * Diccionario con los números del reloj
+     */
+    public static final HashMap<Integer,String> NUMBERS = new HashMap<Integer,String>();
+    static {
+        NUMBERS.put(0,"ZERO");
+        NUMBERS.put(1,"ONE");
+        NUMBERS.put(2,"TWO");
+        NUMBERS.put(3,"THREE");
+        NUMBERS.put(4,"FOUR");
+        NUMBERS.put(5,"FIVE");
+        NUMBERS.put(6,"SIX");
+        NUMBERS.put(7,"SEVEN");
+        NUMBERS.put(8,"EIGHT");
+        NUMBERS.put(9,"NINE");
     }
+
+    /**
+     * Función que devuelve la imagen del número pasado
+     *
+     * @param number número
+     * @return imagen
+     */
+    private Image getImage(int number) {
+        Image imagen = new Image(getClass().getResourceAsStream("/imagenes/" + NUMBERS.get(number) + ".png"));
+        return imagen;
+    }
+
+    /**
+     * Función que se ejecuta cuando se inicia la ventana. Configura el timer del reloj
+     *
+     * @param url
+     * @param resourceBundle
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Timer timer = new Timer("Reloj");
+        timer.scheduleAtFixedRate(new TimerTask() {
+            /**
+             * Función que se ejecuta cuando se lanza la actividad
+             */
+            @Override
+            public void run() {
+                LocalTime ahora = LocalTime.now();
+                // Horas
+                int hora = ahora.getHour();
+                int h1 = hora / 10;
+                hora1.setImage(getImage(h1));
+                int h2 = hora % 10;
+                hora2.setImage(getImage(h2));
+                // Minutos
+                int minuto = ahora.getMinute();
+                int m1 = minuto / 10;
+                min1.setImage(getImage(m1));
+                int m2 = minuto % 10;
+                min2.setImage(getImage(m2));
+                // Segundos
+                int segundo = ahora.getSecond();
+                int s1 = segundo / 10;
+                seg1.setImage(getImage(s1));
+                int s2 = segundo % 10;
+                seg2.setImage(getImage(s2));
+            }
+        }, 0, 1000);
+    }
+
 }
